f0c2026fdd59b80309f8af44ee22beba65d73e81
procrastinat0r <37618509+procrastinat0r@users.noreply.github.com>
2020-05-16 23:14:50 +0200

    Add configuration via configuration file

--- README.md
+++ README.md
@@ -7,7 +7,16 @@ This little Perl script can be used to synchronize files and/or directories usin
 You need the following Perl modules installed:
 - https://metacpan.org/pod/File::ChangeNotify
 - https://metacpan.org/pod/File::Basename
+- https://metacpan.org/pod/AppConfig
 
-If source or destination path is a remote path, then you should ssh-copy-id (1) your public key to the remote host.
+Usage: perl sync.pl [-v[erbose]] [-c[onfig] <config-file>]
 
-It was tested on FreeBSD and Windows 7 (Cygwin).
+Sync.pl is configured via a config file (see syncrc_sample for an example). If
+no config file is specified via command line, then .syncrc in the current
+directory is tried and if this does not exists ~/.syncrc
+
+If the destination path in the config file is a remote path, then you should
+ssh-copy-id (1) your public key to the remote host and also run ssh-agent (1) to
+avoid entering the password for your private SSH key.
+
+Sync.pl was tested on FreeBSD and Windows 7 (Cygwin).
--- sync.pl
+++ sync.pl
@@ -11,75 +11,161 @@ use warnings;
 
 use File::ChangeNotify;
 use File::Basename;
+use AppConfig qw(:expand);
+
+my $verbose = 0;
+my $cfg_file = '';
 
 ######################################################################
-# configuration
-my $verbose = 1;
+sub main
+{
+    check_command_line_arguments();
 
-# Source path. ATTENTION! No trailing / if this is a directory!
-my $src = '/views1/psh/gc21';
-my $src_exclude_file ="$src/gina/.gitignore";
+    unless ($cfg_file)
+    { # no config file specified via comman line ...
+        if (-r '.syncrc') { $cfg_file = '.syncrc' } # ... try .syncrc in current directory
+        elsif (-r '~/.syncrc') { $cfg_file = '~/.syncrc' } # ... try .syncrc in HOME
+        else { error("Can not find any config file\n") }
+    }
 
-# Destination path. ATTENTION! No trailing / if this is a directory!
-# If this is remote path, then you should ssh-copy-id your public key to the remote host
-# so you don't have to enter a password.
-my $dst =  'peter@pluto:/tmp';
-
-# TODO
-# .gitignore files are not fully compliant to rsync exclude files:
-#   A FILE that contains exclude patterns (one per line).  Blank
-#   lines in the file and lines starting with ';' or '#' are
-#   ignored.  If FILE is -, the list will be read from standard
-#   input.
-# So we should filter it and use -
+    check_config_file();
+    initial_full_sync();
+    sync_on_changes();
+}
 
 ######################################################################
-# initial full sync
-my $exclude = '';
-$exclude = "--exclude-from=$src_exclude_file" if -e $src_exclude_file;
-print "Syncing <$src> to <$dst> ...\n" if $verbose;
-my $cmd = "rsync -avPz --delete $exclude $src $dst";
-#print "$cmd\n" if $verbose;
-my $rsp = `$cmd`;
-print "Initial sync done\n" if $verbose;
+sub info
+{
+    my $info = shift;
+    print $info if $verbose;
+}
 
 ######################################################################
-# now sync on changes only
-my $watcher = File::ChangeNotify->instantiate_watcher
-    ( directories => [ $src ],
-      #filter      => qr/\.(?:pm|conf|yml)$/,
-      #exclude     => [ '', '' ],
-      #follow_symlinks => true,
-      sleep_interval => 1, # in seconds
-    );
+sub error
+{
+    my $err = shift;
+    my $with_usage = shift;
+    print "Error! $err";
+    print "Usage: perl sync.pl [-v] [-c <cfg_file>]\n" if $with_usage;
+    exit -1;
+}
 
-print "waiting for changes in $src ...\n" if $verbose;
-$dst = "$dst/" . basename $src;
-while (1) {
-    my @events = $watcher->wait_for_events;
-    for my $event (@events) {
-        my $fn = $event->{path};
-        my $type = $event->{type};
-        my $fn_r = $fn;
-        $fn_r =~ s/$src\///; # make file name relative to src
-        print "$fn ($type)\n" if ($verbose);
-        my $from = '';
-        my $to = '';
-        if ($type eq 'delete')
-        { # if file was deleted sync it's parent directory
-            $fn_r = dirname $fn_r;
-            next unless -e "$src/$fn_r";  # if a directory was deleted then we don't have to sync each of it's childs
-            $from = "$src/$fn_r/";
+sub get_check_path
+{
+    (my $what, my $path, my $is_mandatory, my $need_check, my $need_file) = @_;
+    $path = '' unless defined $path;
+    error("No $what specified\n") if $is_mandatory && !$path;
+    return $path unless $path && $need_check;
+    info("Use $what <$path>\n") if $path;
+    error("Can not find $what <$path>: $!\n") if $need_file && !(-r $path);
+    error("Can not find $what <$path>: $!\n") unless -r $path || -d $path;
+    return $path;
+}
+
+# check command line arguments
+sub check_command_line_arguments
+{
+    my $cfg = AppConfig->new( {
+        PEDANTIC => 1,
+    } );
+    $cfg->define('verbose|v!');
+    $cfg->define('config|c=s');
+    error ("Invalid command line\n", 1) unless $cfg->getopt();
+    $verbose = defined $cfg->verbose ? $cfg->verbose : 0;
+    info("Check command line arguments ...\n");
+    $cfg_file = get_check_path('config file', $cfg->config, 0, 1, 1);
+}
+
+my $src = '';
+my $dst = '';
+my $exclude_from = '';
+my $include_from = '';
+my $gitignore = '';
+
+sub check_config_file
+{
+    my $cfg = AppConfig->new( {
+        PEDANTIC => 1,
+        GLOBAL => {
+            EXPAND => EXPAND_ALL,
         }
-        else
-        {
-            $from = "$src/$fn_r";
+    } );
+    $cfg->define('verbose|v!');
+    $cfg->define('source|src=s');
+    $cfg->define('destination|dst=s');
+    $cfg->define('exclude-from=s');
+    $cfg->define('include-from=s');
+    $cfg->define('gitignore=s');
+    error("Can not read config file <$cfg_file>\n") unless $cfg->file($cfg_file);
+    $verbose = defined $cfg->verbose ? $cfg->verbose : 0;
+    info("Check config file ...\n");
+    $src = get_check_path('source path', $cfg->source, 1, 1, 0);
+    $src =~ s/\/$//; # remove trailing /
+    $dst = get_check_path('destination path', $cfg->destination, 0, 0, 0);
+    $dst =~ s/\/$//; # remove trailing /
+    error("Destination path is empty") unless $dst;
+    $exclude_from = get_check_path('exclude-from file', $cfg->get('exclude-from'), 0, 1, 1);
+    $include_from = get_check_path('include-from file', $cfg->get('include-from'), 0, 1, 1);
+    $gitignore = get_check_path('gitignore file', $cfg->gitignore, 0, 1, 1);
+    error "gitignore not allowed with exclude-from and/or include-from" if $gitignore && ($exclude_from || $include_from);
+    $exclude_from = "--exclude-from=$exclude_from" if $exclude_from;
+    $include_from = "--include-from=$include_from" if $include_from;
+    $gitignore = "--exclude-from=$gitignore" if $gitignore; # TODO add correct handling instead of using it as exclude-from
+}
+
+
+######################################################################
+sub initial_full_sync
+{
+    info "Syncing <$src> to <$dst> ...\n";
+    my $cmd = "rsync -avPz --delete $exclude_from $include_from $gitignore $src $dst";
+    #info "$cmd\n";
+    my $rsp = `$cmd`;
+    info "Initial sync done\n";
+}
+
+######################################################################
+sub sync_on_changes
+{
+
+    my $watcher = File::ChangeNotify->instantiate_watcher
+        ( directories => [ $src ],
+          #filter      => qr/\.(?:pm|conf|yml)$/,
+          #exclude     => [ '', '' ],
+          #follow_symlinks => true,
+          sleep_interval => 1, # in seconds
+        );
+
+    info "waiting for changes in $src ...\n";
+    $dst = "$dst/" . basename $src;
+    while (1) {
+        my @events = $watcher->wait_for_events;
+        for my $event (@events) {
+            my $fn = $event->{path};
+            my $type = $event->{type};
+            my $fn_r = $fn;
+            $fn_r =~ s/$src\///; # make file name relative to src
+            info "$fn ($type)\n";
+            my $from = '';
+            my $to = '';
+            if ($type eq 'delete')
+            { # if file was deleted sync it's parent directory
+                $fn_r = dirname $fn_r;
+                next unless -e "$src/$fn_r";  # if a directory was deleted then we don't have to sync each of it's childs
+                $from = "$src/$fn_r/";
+            }
+            else
+            {
+                $from = "$src/$fn_r";
+            }
+            $to = "$dst/$fn_r";
+            #info "$from -> $to\n";
+            my $cmd = "rsync -avPz --delete $exclude_from $include_from $gitignore $from $to";
+            #info "$cmd\n";
+            my $rsp = `$cmd`;
+            #info "$rsp\n";
         }
-        $to = "$dst/$fn_r";
-        #print "$from -> $to\n" if ($verbose);
-        $cmd = "rsync -avPz --delete $exclude $from $to";
-        #print "$cmd\n" if $verbose;
-        $rsp = `$cmd`;
-        #print "$rsp\n" if $verbose;
     }
 }
+
+main;
